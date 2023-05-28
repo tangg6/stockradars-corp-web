a9b3377dc117486ff000e07fccb2994a4aa20274
Tanabut <tangg6@outlook.com>
2023-05-28 16:03:45 +0700

    Make toggle Language

--- src/components/CorpCard.jsx
+++ src/components/CorpCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Box, Card, Button } from "@mui/material";
+import { Box, Card } from "@mui/material";
 
-const CorpCard = ({ data }) => {
+const CorpCard = ({ data, selectedLanguage }) => {
   const [selectedItem, setSelectedItem] = useState(null);
 
   const handleClick = (item) => {
@@ -14,32 +14,41 @@ const CorpCard = ({ data }) => {
 
   return (
     <Box>
-      <div className="grid grid-cols-3 gap-16">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
         {data.map((item, index) => (
           <div key={index}>
-            <Card variant="outlined" className="p-4">
+            <Card variant="outlined" className="p-4 max-w-sm mx-auto">
+              <h2 className="text-xl font-bold mb-2">{item.N_name}</h2>
+              {/* Shown Detail */}
+              <div>
+                <p className="text-sm mb-1">
+                  {selectedLanguage === "th"
+                    ? item.N_COMPANY_T
+                    : item.N_COMPANY_E}
+                </p>
+                <p className="text-sm mb-1">{item.N_fullname}</p>
+                <p className="text-sm mb-1">{item.N_shortname}</p>
+                <p className="text-sm mb-1">{item.marketcap}</p>
+              </div>
+              {/* Hidden Detail */}
+              {selectedItem === item && (
+                <div>
+                  <p className="text-sm mb-1">{item.F_TYPE}</p>
+                  <p className="text-sm mb-1">
+                    {selectedLanguage === "th"
+                      ? item.N_BUSINESS_TYPE_T
+                      : item.N_BUSINESS_TYPE_E}
+                  </p>
+                  <a href={item.N_URL} className="text-sm mb-1">
+                    {item.N_URL}
+                  </a>
+                </div>
+              )}
               <button
                 onClick={() => handleClick(item)}
-                className="w-full text-left"
+                className="w-full text-center"
               >
-                <h2 className="text-xl font-bold mb-2">{item.N_name}</h2>
-                {/* Shown Detail */}
-                  <div>
-                    <p className="text-sm mb-1">{item.N_fullname}</p>
-                    <p className="text-sm mb-1">{item.N_shortname}</p>
-                    <p className="text-sm mb-1">{item.marketcap}</p>
-                  </div>
-                {/* Hidden Detail */}
-                {selectedItem === item && (
-                  <div>
-                    <p className="text-sm mb-1">{item.N_COMPANY_T}</p>
-                    <p className="text-sm mb-1">{item.N_COMPANY_E}</p>
-                    <p className="text-sm mb-1">{item.F_TYPE}</p>
-                    <p className="text-sm mb-1">{item.N_BUSINESS_TYPE_E}</p>
-                    <p className="text-sm mb-1">{item.N_BUSINESS_TYPE_T}</p>
-                    <p className="text-sm mb-1">{item.N_URL}</p>
-                  </div>
-                )}
+                ...
               </button>
             </Card>
           </div>
@@ -47,7 +56,6 @@ const CorpCard = ({ data }) => {
       </div>
     </Box>
   );
-}
+};
 
-
-export default CorpCard
+export default CorpCard;
--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,24 +1,41 @@
-import CorpCard from '../components/CorpCard';
-import React, { useState, useEffect } from 'react';
-import '../App.css';
+import CorpCard from "../components/CorpCard";
+import React, { useState, useEffect } from "react";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import "../App.css";
 
 const HomePage = () => {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [selectedLanguage, setSelectedLanguage] = useState("th");
 
-    useEffect(() => {
-      fetch('https://stockradars.co/assignment/data.php')
-        .then(response => response.json())
-        .then(data => setData(data))
-        .catch(error => console.error(error));
-    }, []);
-  
+  useEffect(() => {
+    fetch("https://stockradars.co/assignment/data.php")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const setTH = () => {
+    setSelectedLanguage("th");
+  };
+  const setEN = () => {
+    setSelectedLanguage("en");
+  };
+
   return (
-    <>
     <div className="container">
-          <CorpCard data={data} />
-        </div>
-    </>
-  )
-}
+      <div className="toggle-button-group flex justify-end p-2">
+        <ToggleButtonGroup color="primary">
+          <ToggleButton onClick={setEN} sx={{ color: "white" }}>
+            EN
+          </ToggleButton>
+          <ToggleButton onClick={setTH} sx={{ color: "white" }}>
+            TH
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </div>
+      <CorpCard data={data} selectedLanguage={selectedLanguage} />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
